de73102c531dac54d4fcf484dca985dbe3c60ca1
Krishna Rajendran <krishna@emptybox.org>
2024-08-09 14:43:36 -0700

    Generate rest client from root instead of src

--- bin/generate-podman-api.sh
+++ bin/generate-podman-api.sh
@@ -1,5 +1,5 @@
 # Use openapi-client-gen to auto generate the basic podman-autogen-api client
-cargo run -p openapi-client-gen ./swagger/swagger-v5.1.0.modified.yaml ./podman-autogen-api/src
+cargo run -p openapi-client-gen ./swagger/swagger-v5.1.0.modified.yaml ./podman-autogen-api
 
 # Format the auto generated code
 cargo fmt
